94e2918e1c2898ddc8b57659b8c8bbb0743a6de8
young <deroyz@hotmail.com>
2022-05-01 18:23:14 -0600

    First Commit

---
+++ app/src/main/java/com/example/quickstocklist/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.quickstocklist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // A 개발자가 B에게 PUSH 를 날린다.
+}
